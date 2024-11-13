ccde7177c7b3497db14918808a05fcedaa32ad66
Igor Shiota <igorshiota@Igors-MacBook-Pro.local>
2024-11-13 17:28:16 +0000

    First project commit

---
+++ src/test/java/com/ca1/igorspetitions/IgorspetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ca1.igorspetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgorspetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
